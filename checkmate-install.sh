4b7d32a6808671c4f6b882c1e1e776ee168336f2
bryanjpg <57557745+bryanjpg@users.noreply.github.com>
2025-04-20 22:34:42 +0200

    Update checkmate-install.sh

--- checkmate-install.sh
+++ checkmate-install.sh
@@ -27,22 +27,29 @@ start
 build_container
 description
 
-msg_info "Installing Docker & Checkmate..."
-lxc-attach -n $CTID -- bash -c "apt update && apt install -y docker.io docker-compose git locales"
-lxc-attach -n $CTID -- bash -c "locale-gen en_US.UTF-8 && update-locale LANG=en_US.UTF-8"
-lxc-attach -n $CTID -- systemctl enable docker
-lxc-attach -n $CTID -- systemctl start docker
+msg_info "Installing Docker & Checkmate dependencies..."
+lxc-attach -n "$CTID" -- bash -c "apt update && apt install -y docker.io docker-compose git locales curl"
+lxc-attach -n "$CTID" -- bash -c "locale-gen en_US.UTF-8 && update-locale LANG=en_US.UTF-8"
+lxc-attach -n "$CTID" -- systemctl enable docker
+lxc-attach -n "$CTID" -- systemctl start docker
+msg_ok "Docker & dependencies installed."
 
 msg_info "Cloning Checkmate repository..."
-lxc-attach -n $CTID -- bash -c "cd /root && git clone --depth=1 https://github.com/bluewave-labs/Checkmate.git"
-
+lxc-attach -n "$CTID" -- bash -c "cd /root && git clone --depth=1 https://github.com/bluewave-labs/Checkmate.git"
 msg_ok "Checkmate cloned successfully."
-msg_info "Note: No docker-compose.yml found. You may need to start the dev server manually."
 
-msg_info "Suggested next steps (inside LXC):"
-echo -e "${TAB}${INFO} Run:${CL} cd /root/Checkmate && ./scripts/dev.sh"
+msg_info "Downloading docker-compose.yaml..."
+lxc-attach -n "$CTID" -- bash -c "curl -fsSL https://raw.githubusercontent.com/bluewave-labs/Checkmate/refs/heads/master/Docker/dist/docker-compose.yaml -o /root/Checkmate/docker-compose.yaml"
+msg_ok "docker-compose.yaml downloaded."
 
-msg_ok "Completed Successfully!\n"
+msg_info "Next steps:"
+echo -e "${TAB}${INFO} Inside the LXC, run the following to start Checkmate in dev mode:${CL}"
+echo -e "${TAB}${YW}cd /root/Checkmate && docker-compose up -d${CL}"
+echo -e "${TAB}${YW}OR run dev mode: ./scripts/dev.sh${CL}"
+
+msg_ok "Completed Successfully!"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
-echo -e "${INFO}${YW} Access it using the following URL (after dev server starts):${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3000${CL}"
+echo -e "${INFO}${YW} You can access the frontend at:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:5173${CL}"
+echo -e "${INFO}${YW} You can access the backend at:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:5000${CL}"
